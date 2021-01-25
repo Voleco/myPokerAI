e65995f556d40cbc63688d02bd85858f77c2e913
Voleco <chenjingwei1991@gmail.com>
2021-01-25 05:39:13 -0700

    add test for ranking funcitons and hand evaluator

--- apps/gamePlayTest/Driver.cpp
+++ apps/gamePlayTest/Driver.cpp
@@ -8,29 +8,91 @@
 #include"../../environments/GameEngine.h"
 #include"../../environments/Player.h"
 #include"../../environments/RandomPlayer.h"
-
+#include"../../environments/Evaluator.h"
+#include"../../utils/RankingCombinations.h"
 
 using std::cout;
 
+void gameFlowTest();
+void handEvaTest();
+void rankingTest();
+
 int main(int argc,char** argv)
 {
-	/*
-	if (argc > 1 && strcmp(argv[1], "-buildPDB") == 0)
+	if (argc > 1 && strcmp(argv[1], "-testRank") == 0)
 	{
-
-
+		rankingTest();
+	}
+	if (argc > 1 && strcmp(argv[1], "-testHand") == 0)
+	{
+		handEvaTest();
 	}
 
 	else if (argc > 1 && strcmp(argv[1], "-run") == 0)
 	{
-	
+		gameFlowTest();
 	}
 
 	else
 	{
 		std::cout << "Usage: " << argv[0] << " -hehe\n";
 	}
-	*/
+	
+
+
+	
+
+	return 0;
+}
+
+void rankingTest()
+{
+	// /* card to int test*/
+	// Card c1("As");
+	// Card c2("7c");
+	// //expected to be 52 
+	// cout<<"As to int: "<<Card_to_int(c1) <<"\n"; //--- passed
+	// //expected to be 21 
+	// cout<<"7c to int: "<<Card_to_int(c2) <<"\n"; //--- passed
+	
+	// /* int to card test*/
+	// //expected to be As 
+	// cout<<"Card of int 52: "<<Int_to_card(52) <<"\n"; //--- passed
+	// //expected to be 7c  
+	// cout<<"Card of int 21: "<<Int_to_card(21) <<"\n"; //--- passed
+
+	/*ranking function test*/
+	RankingCombination rc;
+	//expect to see 2598960 
+	cout<<"52 choose 5: " <<rc.NChooseK(52,5)<<"\n"; //--- passed
+
+	int* arr = new int[5];
+	for(int i=0;i<5;i++)
+		arr[i] = i;
+	cout<<"rank of [0,1,2,3,4]: " <<rc.Rank(arr,5,52)<<"\n"; //--- passed
+	for(int i=0;i<5;i++)
+		arr[i] = i+47;
+	cout<<"rank of [47,48,49,50,51]: " <<rc.Rank(arr,5,52)<<"\n"; //--- passed
+
+	for(int i=0;i<5;i++)
+		arr[i] = 0;
+	rc.Unrank(2598959,arr,5,52);
+	cout<<"result of unrank 2598959:";
+	for(int i=0;i<5;i++)
+		cout<<arr[i]<<",";
+	cout<<"\n"; //--- passed
+}
+void handEvaTest()
+{
+
+
+
+	 HandEvaluator he;
+	 he.BuildDATFile();
+}
+
+void gameFlowTest()
+{
 	GameEngine eng;
 	eng.SetTableSize(8);
 	eng.ClearTable();
@@ -79,8 +141,4 @@ int main(int argc,char** argv)
 				eng.playerPtrs[i]->PrintPlayerInfo();
 		}
 	}
-
-	
-
-	return 0;
 }
